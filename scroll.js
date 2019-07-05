2f759c4d7285c76475a3f1a4f4d13d8bf354bdc0
Luca Mariotti <mariotti93@gmail.com>
2019-07-05 09:21:14 +0200

    Add files via upload

---
+++ scroll.js
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <style>
+        #my-scrollbar {
+           
+            overflow: auto;
+        }
+    </style>
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container" id="my-scrollbar">
+        <h1>mngdksmnglksmkl</h1>
+        <h1>mngdksmnglksmkl</h1>
+        <h1>mngdksmnglksmkl</h1>
+        <h1>mngdksmnglksmkl</h1>
+        <h1>mngdksmnglksmkl</h1>
+        <h1>mngdksmnglksmkl</h1>
+        <h1>mngdksmnglksmkl</h1>
+        <h1>mngdksmnglksmkl</h1>
+        <h1>mngdksmnglksmkl</h1>
+        <h1>mngdksmnglksmkl</h1>
+        <h1>mngdksmnglksmkl</h1>
+        <h1>mngdksmnglksmkl</h1>
+        <h1>mngdksmnglksmkl</h1>
+        <h1>mngdksmnglksmkl</h1>
+        <h1>mngdksmnglksmkl</h1>
+        <h1>mngdksmnglksmkl</h1>
+        <h1>mngdksmnglksmkl</h1>
+        <h1>mngdksmnglksmkl</h1>
+        <h1>mngdksmnglksmkl</h1>
+        <h1>mngdksmnglksmkl</h1>
+        <h1>mngdksmnglksmkl</h1>
+        <h1>mngdksmnglksmkl</h1>
+        <h1>mngdksmnglksmkl</h1>
+        <h1>mngdksmnglksmkl</h1>
+        <h1>mngdksmnglksmkl</h1>
+        <h1>mngdksmnglksmkl</h1>
+        <h1>mngdksmnglksmkl</h1>
+        <h1>mngdksmnglksmkl</h1>
+        <h1>mngdksmnglksmkl</h1>
+        <h1>mngdksmnglksmkl</h1>
+        <h1>mngdksmnglksmkl</h1>
+        <h1>mngdksmnglksmkl</h1>
+        <h1>mngdksmnglksmkl</h1>
+        <h1>mngdksmnglksmkl</h1>
+        <h1>mngdksmnglksmkl</h1>
+        <h1>mngdksmnglksmkl</h1>
+        <h1>mngdksmnglksmkl</h1>
+        <h1>mngdksmnglksmkl</h1>
+        <h1>mngdksmnglksmkl</h1>
+        <h1>mngdksmnglksmkl</h1>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/smooth-scrollbar/8.4.0/smooth-scrollbar.js"></script>
+    <script>
+        var Scrollbar = window.Scrollbar;
+
+        Scrollbar.init(document.querySelector('#my-scrollbar'), {
+            damping: 0.01,
+        });
+    </script>
+</body>
+
+</html>
